8731b4adad6247aa8d5555d939b45d1b33d9b1e0
m7mark <m7mark@yandex.ru>
2021-08-10 15:06:54 +0300

    Button & Input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('')
+
+  const increment = () => { setLikes(likes + 1) }
+  function decrement() { setLikes(likes - 1) }
+
   return (
     <div>
-      App is working!
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)} />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
