ee3e066d0a753b00ee2026b48932e023323bda78
m7mark <m7mark@yandex.ru>
2021-08-10 19:53:39 +0300

    Posts FC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
+import React, { useState } from 'react';
+import PostsList from './components/PostsList';
+// import ClassCounter from './components/ClassCounter';
+// import Counter from './components/Counter';
+import './styles/App.css'
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 0', body: 'Description' },
+    { id: 2, title: 'JavaScript 1', body: 'Description' },
+    { id: 3, title: 'JavaScript 2', body: 'Description' }
+  ]);
+
   return (
-    <div>
-      <Counter />
-      <ClassCounter />
+    <div className='App'>
+      < PostsList posts={posts} title="JS posts"/>
     </div>
   );
 }
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PostItem from './PostItem';
+
+const PostsList = ({posts, title}) => {
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>
+                {title}
+            </h1>
+            {posts.map(post =>
+                <PostItem post={post} key={post.id} />
+            )}
+        </div>
+    );
+}
+
+export default PostsList;
