f985a71441b10771cb0c79fbd92ac898d7c71c92
m7mark <m7mark@yandex.ru>
2021-08-11 01:44:12 +0300

    React Transition Group - Animation

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PostItem from './PostItem';
 
 const PostsList = ({ posts, title, remove }) => {
@@ -12,12 +13,21 @@ const PostsList = ({ posts, title, remove }) => {
             <h1 style={{ textAlign: 'center' }}>
                 {title}
             </h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove}
-                    number={index + 1}
-                    post={post}
-                    key={post.id} />
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem
+                            remove={remove}
+                            number={index + 1}
+                            post={post}
+                        />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 }
