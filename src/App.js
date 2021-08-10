3e86f3d601c92534bee26e9b00ac44b45962c858
m7mark <m7mark@yandex.ru>
2021-08-10 22:27:24 +0300

    Post Form refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
+import PostForm from './components/PostForm';
 import PostsList from './components/PostsList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
 // import ClassCounter from './components/ClassCounter';
 // import Counter from './components/Counter';
 import './styles/App.css'
@@ -12,37 +11,16 @@ function App() {
     { id: 2, title: 'JavaScript 1', body: 'Description' },
     { id: 3, title: 'JavaScript 2', body: 'Description' }
   ]);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title, body
-    }
+  const createPost = (newPost) => {
     setPosts([...posts, newPost])
-    setTitle('')
-    setBody('')
   }
 
 
 
   return (
     <div className='App'>
-      <form>
-        <MyInput
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          type="text"
-          placeholder="Post name" />
-        <MyInput
-          value={body}
-          onChange={e => setBody(e.target.value)}
-          type="text"
-          placeholder="Post Description" />
-        <MyButton onClick={addNewPost}>Create post</MyButton>
-      </form>
+      <PostForm create={createPost} />
       < PostsList posts={posts} title="JS posts" />
     </div>
   );
