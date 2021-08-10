b775f7e7937ab98eae1dac52460d7e1793290d3c
m7mark <m7mark@yandex.ru>
2021-08-11 00:00:05 +0300

    Sort Posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PostForm from './components/PostForm';
 import PostsList from './components/PostsList';
+import MySelect from './components/UI/select/MySelect';
 // import ClassCounter from './components/ClassCounter';
 // import Counter from './components/Counter';
 import './styles/App.css'
@@ -8,20 +9,41 @@ import './styles/App.css'
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: 'JavaScript 0', body: 'Description' },
-    { id: 2, title: 'JavaScript 1', body: 'Description' },
-    { id: 3, title: 'JavaScript 2', body: 'Description' }
+    { id: 2, title: 'NJavaScript 1', body: 'ADescription' },
+    { id: 3, title: 'BJavaScript 2', body: 'BDescription' }
   ]);
 
+  // callback
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+  const [selectedSort, setSelectedSort] = useState('');
 
-
+  const sortProps = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className='App'>
       <PostForm create={createPost} />
-      < PostsList posts={posts} title="JS posts" />
+      <hr style={{ margin: '15px 0' }} />
+      <div><MySelect
+        defaultValue="Sort"
+        options={[
+          { value: 'title', name: 'By name' },
+          { value: 'body', name: 'By description' }
+        ]}
+        value={selectedSort}
+        onChange={sortProps}
+      /></div>
+      {posts.length
+        ? < PostsList remove={removePost} posts={posts} title="JS posts" />
+        : <h1 style={{ textAlign: 'center' }}> Post list is Empty</h1>}
+
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import MyButton from './UI/button/MyButton';
 
 const PostItem = (props) => {
     return (
         <div className='post'>
             <div className='post__content'>
                 <strong>{props.number}.{props.post.title}</strong>
-                <div>
-                    {props.post.body}
-                </div>
+                <div>{props.post.body}</div>
             </div>
             <div>
-                <button>Delete</button>
+                <MyButton onClick={() => props.remove(props.post)}>
+                    Delete
+                </MyButton>
             </div>
         </div>
     );
