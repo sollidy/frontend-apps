f4e1b2f8695ddfe86a244c5bbcb7ab54476f74f2
m7mark <m7mark@yandex.ru>
2021-08-29 22:37:05 +0300

    Base styles of Posts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-      <div>
-          <button @click="addLike">Add Like</button> 
-          <button @click="addDislike">Add dislikes</button> 
-      </div>
-    <div>
-      likes <strong>{{ likes }}</strong>
-    </div>
-    <div>
-      dislikes <strong>{{ dislikes }}</strong>
+  <div class='app'>
+    <form>
+        <h4>New Post</h4>
+      <input class='input' type="text" placeholder="Title" />
+      <input class='input' type="text" placeholder="Description" />
+      <button class='btn' @click="createPost">Create</button>
+    </form>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+      <div><strong>Title:</strong>{{ post.title }}</div>
+      <div><strong>Description:</strong>{{ post.body }}</div>
     </div>
   </div>
 </template>
@@ -17,20 +17,51 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        { id: 1, title: "JS 1", body: "about this" },
+        { id: 3, title: "JS 2", body: "about this" },
+        { id: 4, title: "JS 3", body: "about this" },
+      ],
     };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDislike() {
-      this.dislikes += 1;
-    },
+      createPost() {
+          
+      }
   },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.app{
+    padding: 20px 20px;
+}
+form {
+    display: flex;
+    flex-direction: column;
+}
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal
+}
 </style>
