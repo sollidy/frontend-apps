159cda7d84d5dbc700ffdc90d253827a9e268367
m7mark <m7mark@yandex.ru>
2021-08-29 22:08:11 +0300

    Counter

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <div>
+          <button @click="addLike">Add Like</button> 
+          <button @click="addDislike">Add dislikes</button> 
+      </div>
+    <div>
+      likes <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      dislikes <strong>{{ dislikes }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
