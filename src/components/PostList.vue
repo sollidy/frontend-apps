84d76d3c4c3024eb49fa03e3e2cb02de080ef5e5
m7mark <m7mark@yandex.ru>
2021-09-01 21:50:22 +0300

    Animate posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Posts List:</h3>
-    <post-item @remove="$emit('remove',post)"
-    :post="post" v-for="post in posts" :key="post.id" />
+    <transition-group name="list">
+      <post-item
+        @remove="$emit('remove', post)"
+        :post="post"
+        v-for="post in posts"
+        :key="post.id"
+      />
+    </transition-group>
   </div>
   <h2 v-else>List is empty!</h2>
 </template>
@@ -25,5 +31,21 @@ export default {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 15px;
+}
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.4s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(150px);
+}
+.list-move {
+  transition: transform 0.4s ease;
 }
 </style>
