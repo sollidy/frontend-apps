84c97c1687a03748e87a9e913e9f30490b1ba37c
m7mark <m7mark@yandex.ru>
2021-09-04 20:23:10 +0300

    Pagination

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app">
     <h1>Posts</h1>
+    <my-input v-model="searchQuery" placeholder="Find" />
     <div class="app__btns">
       <my-button @click="showDialog">Create new post</my-button>
       <my-select v-model="selectedSort" :options="sortOptions" />
@@ -11,8 +12,23 @@
     </my-dialog>
 
     <post-form @create="createPost" />
-    <post-list @remove="removePost" :posts="sortedPosts" v-if="!isPostsLoading" />
+    <post-list
+      @remove="removePost"
+      :posts="sortedAndSearchedPosts"
+      v-if="!isPostsLoading"
+    />
     <div v-else>Loading...</div>
+    <div class="page__wrapper">
+      <div
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        class="page"
+        :class="{ 'current-page': page === pageNumber }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +38,7 @@ import PostList from "@/components/PostList";
 import MyDialog from "./components/UI/MyDialog.vue";
 import MyButton from "./components/UI/MyButton.vue";
 import axios from "axios";
+import MyInput from "./components/UI/MyInput.vue";
 
 export default {
   components: {
@@ -29,6 +46,7 @@ export default {
     PostList,
     MyDialog,
     MyButton,
+    MyInput,
   },
   data() {
     return {
@@ -36,6 +54,10 @@ export default {
       dialogVisible: false,
       isPostsLoading: false,
       selectedSort: "",
+      searchQuery: "",
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         { value: "title", name: "By title" },
         { value: "body", name: "By description" },
@@ -53,11 +75,23 @@ export default {
     showDialog() {
       this.dialogVisible = true;
     },
+    changePage(pageNumber) {
+      this.page = pageNumber;
+    },
     async fetchPosts() {
       this.isPostsLoading = true;
       try {
         const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
         );
         this.posts = response.data;
       } catch (e) {
@@ -76,14 +110,17 @@ export default {
         post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
       );
     },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
   },
-  // watch: {
-  //   selectedSort(newValue) {
-  //     this.posts.sort((post1, post2) => {
-  //       return post1[newValue]?.localeCompare(post2[newValue]);
-  //     });
-  //   },
-  // },
+  watch: {
+    page() {
+      this.fetchPosts();
+    },
+  },
 };
 </script>
 
@@ -100,5 +137,16 @@ export default {
 }
 .app {
   padding: 20px 20px;
+}
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 2px solid green;
 }
 </style>
