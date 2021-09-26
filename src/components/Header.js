c5fddec96c4efde3765c68ffbcb258d232f9dd84
m7mark <m7mark@yandex.ru>
2021-09-26 21:56:05 +0300

    NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
-import { StyledHeader } from "./styled/Header.styled";
+import { Button } from "./styled/Button.styled";
+import { Container } from "./styled/Container.styled";
+import { Flex } from "./styled/Flex.styled";
+import { Image, Logo, Nav, StyledHeader } from "./styled/Header.styled";
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <h1>Head</h1>
-
+            <Container>
+                <Nav>
+                    <Logo src='./images/logo.svg' alt='' />
+                    <Button>Try it free</Button>
+                </Nav>
+                <Flex>
+                    <div>
+                        <h1>Build The Community Your Fans Will Love</h1>
+                        <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+                        <Button bg='#ff0099' color='#fff'>Get started for free</Button>
+                    </div>
+                    <Image src='./images/illustration-mockups.svg' alt='' />
+                </Flex>
+            </Container>
         </StyledHeader>
     );
 };
