dd1d830f4ff510f428fb9be31227ad624aaf4ab1
m7mark <m7mark@yandex.ru>
2021-09-26 21:00:23 +0300

    Theme provider & Global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import { Container } from "./components/styled/Container.styled";
+import GlobalStyles from "./components/styled/Global";
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+      <GlobalStyles />
+        <Header />
+        <Container>
+          <h1>Hello</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { StyledHeader } from "./styled/Header.styled";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <h1>Head</h1>
+
+        </StyledHeader>
+    );
+};
+
+export default Header;
