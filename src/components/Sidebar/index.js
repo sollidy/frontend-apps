c255e011143dc4132be09ec48d25b2497832af95
m7mark <m7mark@yandex.ru>
2021-09-29 00:11:28 +0300

    Sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import { CloseIcon, Icon, SidebarContainer, SidebarLink, SidebarMenu, SidebarRoute, SidebarWrapper, SideBtnWrap } from "./SidebarElements";
+
+export const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon/>
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to='about'>
+                        About
+                    </SidebarLink>
+                    <SidebarLink to='discover'>
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to='services'>
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to='signup'>
+                        Sign Up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to='/signin'>Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    );
+};
