7e80999fa734da704576f663b9740359417139fe
m7mark <m7mark@yandex.ru>
2021-09-28 12:23:09 +0300

    Find post - toLowerCase

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -14,7 +14,7 @@ export const useSortedPosts = (posts, sort) => {
 export const usePosts = (posts, sort, query) => {
     const sortedPosts = useSortedPosts(posts, sort)
     const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(query))
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()))
     }, [query, sortedPosts])
     return sortedAndSearchedPosts
 }
