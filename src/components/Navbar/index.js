79d60aeb3ef315b686a36f80237e6f7d20dccef3
m7mark <m7mark@yandex.ru>
2021-09-28 22:29:50 +0300

    NavBar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,34 @@
+import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLink, NavLogo, NavMenu } from "./NavbarElements";
+import { FaBars } from 'react-icons/fa'
+
+export const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'> mark.</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLink to='about'>About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='discover'>Discover</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='services'>Services</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='signup'>Sign Up</NavLink>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
