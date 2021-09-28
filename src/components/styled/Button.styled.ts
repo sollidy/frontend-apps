0c40369782c1723b13f753b5def5bfa0f8c8c10a
m7mark <m7mark@yandex.ru>
2021-09-28 15:55:28 +0300

    Public index

--- src/components/styled/Button.styled.ts
+++ src/components/styled/Button.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Button = styled.button<{ bg?: string, color?: string }>`
     border-radius: 50px;
     border: none;
-    box-shadow: 0 0 10px rgba(0 0 0 0.15);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
     font-size: 16px;
     font-weight: 700;
     padding: 15px 60px;
