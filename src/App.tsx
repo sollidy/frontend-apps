ea31e7c2482bf96ade238bd1781b2243f15b9aa7
m7mark <m7mark@yandex.ru>
2022-06-07 12:11:48 +0300

    Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Character } from './pages/Character'
 import { CharactersList } from './pages/CharactersList'
+import { Main } from './pages/Main'
 
 function App() {
   return (
     <div className="container mx-auto px-4">
       <Routes>
-        <Route path="/" element={<CharactersList />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/characters" element={<CharactersList />} />
         <Route path="/:id" element={<Character />} />
       </Routes>
     </div>
