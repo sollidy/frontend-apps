2047d1c348d91b8d67fdc2918de926ff6531c133
m7mark <m7mark@yandex.ru>
2022-06-07 18:01:57 +0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
+import { Header } from './components/Header'
 import { Character } from './pages/Character'
 import { CharactersList } from './pages/CharactersList'
 import { Main } from './pages/Main'
 
 function App() {
   return (
-    <div className="container mx-auto px-4">
+    <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/characters" element={<CharactersList />} />
--- src/pages/CharactersList.tsx
+++ src/pages/CharactersList.tsx
@@ -1,21 +1,29 @@
 import { Link } from 'react-router-dom'
+import { HeaderMenu } from '../components/HeaderMenu'
 import { useCharacters } from '../hooks/useCharacters'
 
 export const CharactersList = () => {
   const { error, data, loading } = useCharacters()
-  if (loading) return <div>Loading...</div>
+  // if (loading) return <></>
   if (error) return <div>Network error</div>
   return (
     <>
-      <div>
-        {data.characters.results.map((character: any) => {
-          return (
-            <Link to={`/${character.id}`} key={character.id}>
-              <img src={character.image} alt="a" />
-              <h2>{character.name}</h2>
-            </Link>
-          )
-        })}
+      <div className="relative bg-white overflow-hidden">
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-31">
+            <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 ">
+              {data?.characters &&
+                data.characters.results.map((character: any) => {
+                  return (
+                    <Link to={`/${character.id}`} key={character.id}>
+                      <img src={character.image} alt="a" />
+                      <h2>{character.name}</h2>
+                    </Link>
+                  )
+                })}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
