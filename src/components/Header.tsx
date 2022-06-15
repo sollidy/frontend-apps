d2ddad2642b03d9f56a1f9e2759c88daa0ef4c7d
m7mark <m7mark@yandex.ru>
2022-06-15 23:45:10 +0300

    close after click menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -87,30 +87,35 @@ export function Header() {
                     leaveTo="opacity-0 translate-y-1"
                   >
                     <Popover.Panel className="absolute z-20 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-44">
-                      <div className="rounded-lg shadow-lg ring-1 ring-black dark:ring-gray-500 ring-opacity-5 overflow-hidden">
-                        <div className="relative grid gap-6 bg-white dark:bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
-                          {source.map((item) => (
-                            <div
-                              key={item.name}
-                              onClick={() => navigate(item.href)}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 cursor-pointer"
-                            >
-                              <item.icon
-                                className="flex-shrink-0 h-6 w-6 text-green-600"
-                                aria-hidden="true"
-                              />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900 dark:text-gray-100">
-                                  {item.name}
-                                </p>
-                                <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-                                  {item.description}
-                                </p>
+                      {({ close }) => (
+                        <div className="rounded-lg shadow-lg ring-1 ring-black dark:ring-gray-500 ring-opacity-5 overflow-hidden">
+                          <div className="relative grid gap-6 bg-white dark:bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
+                            {source.map((item) => (
+                              <div
+                                key={item.name}
+                                onClick={() => {
+                                  navigate(item.href)
+                                  close()
+                                }}
+                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 cursor-pointer"
+                              >
+                                <item.icon
+                                  className="flex-shrink-0 h-6 w-6 text-green-600"
+                                  aria-hidden="true"
+                                />
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-900 dark:text-gray-100">
+                                    {item.name}
+                                  </p>
+                                  <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+                                    {item.description}
+                                  </p>
+                                </div>
                               </div>
-                            </div>
-                          ))}
+                            ))}
+                          </div>
                         </div>
-                      </div>
+                      )}
                     </Popover.Panel>
                   </Transition>
                 </>
@@ -139,48 +144,58 @@ export function Header() {
           focus
           className="absolute z-20 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
         >
-          <div className="rounded-lg shadow-lg ring-1 ring-black dark:ring-gray-500 ring-opacity-5 bg-white dark:bg-gray-800 divide-y-2 divide-gray-50 dark:divide-gray-700">
-            <div className="pt-5 pb-6 px-5">
-              <div className="flex items-center justify-between">
-                <div className="font-bold text-2xl dark:text-gray-300">r&m</div>
-                <div className="-mr-2">
-                  <Popover.Button className="bg-white dark:bg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 dark:hover:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
-                    <span className="sr-only">Close menu</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
+          {({ close }) => (
+            <div className="rounded-lg shadow-lg ring-1 ring-black dark:ring-gray-500 ring-opacity-5 bg-white dark:bg-gray-800 divide-y-2 divide-gray-50 dark:divide-gray-700">
+              <div className="pt-5 pb-6 px-5">
+                <div className="flex items-center justify-between">
+                  <div className="font-bold text-2xl dark:text-gray-300">
+                    r&m
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white dark:bg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 dark:hover:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid gap-y-8">
+                    {source.map((item) => (
+                      <div
+                        key={item.name}
+                        onClick={() => {
+                          navigate(item.href)
+                          close()
+                        }}
+                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 dark:hover:bg-gray-700 cursor-pointer"
+                      >
+                        <item.icon
+                          className="flex-shrink-0 h-6 w-6 text-green-600"
+                          aria-hidden="true"
+                        />
+                        <span className="ml-3 text-base font-medium text-gray-900 dark:text-gray-100">
+                          {item.name}
+                        </span>
+                      </div>
+                    ))}
+                  </nav>
                 </div>
               </div>
-              <div className="mt-6">
-                <nav className="grid gap-y-8">
-                  {source.map((item) => (
-                    <div
-                      key={item.name}
-                      onClick={() => navigate(item.href)}
-                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 dark:hover:bg-gray-700 cursor-pointer"
-                    >
-                      <item.icon
-                        className="flex-shrink-0 h-6 w-6 text-green-600"
-                        aria-hidden="true"
-                      />
-                      <span className="ml-3 text-base font-medium text-gray-900 dark:text-gray-100">
-                        {item.name}
-                      </span>
-                    </div>
-                  ))}
-                </nav>
-              </div>
-            </div>
-            <div className="py-6 px-5 space-y-6">
-              <div>
-                <div
-                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700 cursor-pointer"
-                  onClick={() => navigate('/')}
-                >
-                  Sign up
+              <div className="py-6 px-5 space-y-6">
+                <div>
+                  <div
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700 cursor-pointer"
+                    onClick={() => {
+                      navigate('/')
+                      close()
+                    }}
+                  >
+                    Sign up
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </Popover.Panel>
       </Transition>
     </Popover>
