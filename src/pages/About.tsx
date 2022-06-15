7ad7a7adbc633373db4da3c3a0bc6804eaf60349
m7mark <m7mark@yandex.ru>
2022-06-15 14:52:00 +0300

    About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,14 @@
 export const About = () => {
   return (
-    <>
-      <div>About</div>
-    </>
+    <div>
+      <div className="flex mb-8 px-4 sm:px-6 lg:px-8 lg:mt-6 flex-col items-center">
+        <h1 className="mt-6 sm:mt-10 lg:mt-13 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
+          Technologies
+        </h1>
+        <p className="mt-4 text-base text-gray-500 dark:text-gray-400 sm:mt-7 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
+          React, TypeScript, TailWind, GraphQL
+        </p>
+      </div>
+    </div>
   )
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -17,8 +17,8 @@ export const Main = () => {
         </svg>
         <main className="pt-10 mx-auto max-w-7xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28 ">
           <div className="sm:text-center lg:text-left">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block xl:inline dark:text-gray-50">Rick & </span>{' '}
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
+              <span className="block xl:inline">Rick & </span>{' '}
               <span className="block text-green-600 xl:inline">Morty</span>
             </h1>
             <p className="mt-3 text-base text-gray-500 dark:text-gray-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
